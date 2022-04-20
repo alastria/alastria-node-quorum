cd210ede9b47f0460ff0cdd9601fd0aa13ed1364
Iker Ruiz de Infante Gonzalez <iker@alastria.io>
2022-04-20 11:59:34 +0200

    Added ethstats metrics

--- docker-compose/alastria-node-data/env/geth.node.general.sh
+++ docker-compose/alastria-node-data/env/geth.node.general.sh
@@ -5,6 +5,7 @@ NODE_ARGS=" --rpc --rpcaddr 127.0.0.0 --rpcapi admin,db,eth,debug,miner,net,shh,
 
 # The Grafana server for pulling metrics. tcp/6060 should be opened
 # METRICS=" --metrics --pprof --pprof.addr=0.0.0.0"
+METRICS=" --ethstats $NODE_NAME:bb98a0b6442386d0cdf8a31b267892c1@netstats.telsius.alastria.io:80"
 
 # Example - Enable WS connections
 # NODE_ARGS=${NODE_ARGS}" --ws --wsaddr 127.0.0.0 --wsport 22001 --wsorigins source.com"
