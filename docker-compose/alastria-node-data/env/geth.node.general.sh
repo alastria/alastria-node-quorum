fe0e077996fd7ffc89e9f80941593bd8847d28d3
Iker Ruiz de Infante Gonzalez <iker@alastria.io>
2022-05-24 09:58:28 +0200

    Allow WS connection from outside of container

--- docker-compose/alastria-node-data/env/geth.node.general.sh
+++ docker-compose/alastria-node-data/env/geth.node.general.sh
@@ -8,4 +8,4 @@ NODE_ARGS=" --rpc --rpcaddr 0.0.0.0 --rpcapi admin,db,eth,debug,miner,net,shh,tx
 METRICS=" --ethstats $NODE_NAME:bb98a0b6442386d0cdf8a31b267892c1@netstats.telsius.alastria.io:80"
 
 # Example - Enable WS connections
-# NODE_ARGS=${NODE_ARGS}" --ws --wsaddr 127.0.0.1 --wsport 22001 --wsorigins source.com"
+# NODE_ARGS=${NODE_ARGS}" --ws --wsaddr 0.0.0.0 --wsport 22001 --wsorigins source.com"
