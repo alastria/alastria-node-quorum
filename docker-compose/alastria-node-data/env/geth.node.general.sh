96e99d605aa263b6e2ce9f51e7a65cbda355f6a4
Iker Ruiz de Infante Gonzalez <iker@alastria.io>
2022-06-16 14:53:33 +0200

    Added Prometheus metrics options for regular nodes

--- docker-compose/alastria-node-data/env/geth.node.general.sh
+++ docker-compose/alastria-node-data/env/geth.node.general.sh
@@ -3,8 +3,8 @@
 # Enable RPC connections
 NODE_ARGS=' --rpc --rpcaddr 0.0.0.0 --rpcport 22000 --rpccorsdomain "*" --rpcvhosts "*" --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,istanbul'
 
-# The Ethstats server where to send the info
-METRICS=" --ethstats $NODE_NAME:bb98a0b6442386d0cdf8a31b267892c1@netstats.telsius.alastria.io:80"
+# For pulling metrics from Prometheus/Grafana server. tcp/6060 should be opened
+METRICS="--metrics --metrics.expensive --pprof --pprofaddr=0.0.0.0"
 
 # Enable WS connections
 NODE_ARGS=${NODE_ARGS}' --ws --wsaddr 0.0.0.0 --wsport 22001 --wsorigins "*" --wsapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,istanbul'
