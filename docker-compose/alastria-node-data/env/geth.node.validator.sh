9b487c674adf57098c0577039cd98cd4344c597e
Iker Ruiz de Infante Gonzalez <iker@alastria.io>
2022-06-21 17:01:00 +0200

    Updated deprecated option --minerthreads

--- docker-compose/alastria-node-data/env/geth.node.validator.sh
+++ docker-compose/alastria-node-data/env/geth.node.validator.sh
@@ -1,5 +1,5 @@
 # validator ARGS for validator node
-NODE_ARGS=" --maxpeers 32 --mine --miner.gastarget 8000000 --miner.gaslimit 10000000 --minerthreads $(grep -c "processor" /proc/cpuinfo) --miner.extradata $NODE_NAME"
+NODE_ARGS=" --maxpeers 32 --mine --miner.gastarget 8000000 --miner.gaslimit 10000000 --miner.threads $(grep -c "processor" /proc/cpuinfo) --miner.extradata $NODE_NAME"
 
 # For pulling metrics from Prometheus/Grafana server. tcp/6060 should be opened
 METRICS="--metrics --metrics.expensive --pprof --pprofaddr=0.0.0.0"
